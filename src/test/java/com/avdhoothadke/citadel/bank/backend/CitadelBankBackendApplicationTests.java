402fd34c7a38b1880efa7d5af05fd66910ca7494
avdhoot <avdhoothadke@gmail.com>
2025-11-18 22:26:48 +0530

    INIT

---
+++ src/test/java/com/avdhoothadke/citadel/bank/backend/CitadelBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avdhoothadke.citadel.bank.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitadelBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
